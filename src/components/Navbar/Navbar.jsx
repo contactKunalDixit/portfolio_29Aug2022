3e711f666cd5f3b474397809a669462b28c9ff5e
Kunal Dixit <contactkunaldixit@gmail.com>
2023-03-22 16:55:43 +0800

    Updated: Navbar Navlink isActive Links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,26 +9,61 @@ const Navbar = () => {
 				<ul className={styles.nav_ul}>
 					{/* <div className={styles.highlight}> */}
 					<li className={styles.nav_list}>
-						<NavLink to='/'>Welcome</NavLink>
+						<NavLink
+							to='/'
+							style={({ isActive }) => {
+								return isActive ? { color: 'rgb(235, 199, 205)' } : {};
+							}}
+						>
+							Welcome
+						</NavLink>
 					</li>
 					<div className={styles.nav_hr} />
 					{/* </div>   */}
 					<li className={styles.nav_list}>
-						<NavLink to='/services'>What I Do</NavLink>
+						<NavLink
+							to='/services'
+							style={({ isActive }) => {
+								return isActive ? { color: 'rgb(235, 199, 205)' } : {};
+							}}
+						>
+							What I Do
+						</NavLink>
 					</li>
 					<div className={styles.nav_hr} />
 					<li className={styles.nav_list}>
-						<NavLink to='/mywork'>My Work</NavLink>
+						<NavLink
+							to='/mywork'
+							style={({ isActive }) => {
+								return isActive ? { color: 'rgb(235, 199, 205)' } : {};
+							}}
+						>
+							My Work
+						</NavLink>
 					</li>
 					<div className={styles.nav_hr} />
 
 					<li className={styles.nav_list}>
-						<NavLink to='/testimonials'>Testimonials</NavLink>
+						<NavLink
+							to='/testimonials'
+							style={({ isActive }) => {
+								return isActive ? { color: 'rgb(235, 199, 205)' } : {};
+							}}
+						>
+							Testimonials
+						</NavLink>
 					</li>
 					<div className={styles.nav_hr} />
 
 					<li className={styles.nav_list}>
-						<NavLink to='/contact'>Contact</NavLink>
+						<NavLink
+							to='/contact'
+							style={({ isActive }) => {
+								return isActive ? { color: 'rgb(235, 199, 205)' } : {};
+							}}
+						>
+							Contact
+						</NavLink>
 					</li>
 					<div className={styles.nav_hr} />
 				</ul>
